2192cc6b633207910eb4079565e12cd1763e63c7
anshika21gulati <85219676+anshika21gulati@users.noreply.github.com>
2021-07-15 11:30:59 +0530

    Delete change_delimiter.py

--- change_delimiter.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-reader = csv.reader(open("spam.csv", "rU"), delimiter=',')
-writer = csv.writer(open("spam.txt", 'w'), delimiter=';')
-writer.writerows(reader)
-
-print("Delimiter successfully changed")
